1b65df64ce65e30650870d0c2989a1013d9bba02
박관우 <pgw9470@naver.com>
2020-05-01 23:44:45 +0900

    c++ 컴파일 구현

---
+++ compiletest/src/main/java/com/compile/compiletest/Run/RunCpp.java
@@ -0,0 +1,144 @@
+package com.compile.compiletest.Run;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class RunCpp {
+	
+	private StringBuffer buffer;
+	private Process process;
+	private BufferedReader bufferedReader;
+	private StringBuffer readBuffer;
+	
+	private File file;
+	private BufferedWriter bufferWriter;
+	
+	private final String FILENAME = "cppTest.cpp";
+	
+	public String inputSource(String source) {
+		
+		buffer = new StringBuffer();
+		
+		buffer.append("cmd.exe ");
+		buffer.append("/c ");
+		buffer.append("g++ -o cppTest.exe cppTest.cpp 2>errCpp.txt");
+		
+		createFileAsSource(source);
+		
+		return buffer.toString();
+	}
+	
+	public String errorResult() {
+		String errorResult = "";
+		try {
+			File file2 = new File("errCpp.txt");
+			
+			Scanner scan = new Scanner(file2);
+			
+			while(scan.hasNextLine()) {
+				errorResult += scan.nextLine() + "\n";
+//				System.out.println(scan.nextLine());
+			}
+			System.out.println(errorResult);
+			
+			
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+		return errorResult;
+	}
+	
+	private void createFileAsSource(String source) {
+		try {
+			file = new File(FILENAME);
+			bufferWriter = new BufferedWriter(new FileWriter(file, false));
+			
+			bufferWriter.write(source);
+			bufferWriter.flush();
+		} catch(Exception e) {
+			e.printStackTrace();
+			System.exit(1);
+		} finally {
+			try {
+				bufferWriter.close();
+				file = null;
+			} catch (IOException e) {
+				e.printStackTrace();
+				System.exit(1);;
+			}
+		}
+	}
+	
+	public String execCommand() {
+		try {
+//			process = Runtime.getRuntime().exec(cmd);
+			process = Runtime.getRuntime().exec(runClass());
+			
+			bufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+			String line = null;
+			readBuffer = new StringBuffer();
+			
+			while((line = bufferedReader.readLine()) != null) {
+				readBuffer.append(line);
+				readBuffer.append("\n");
+			}
+			return readBuffer.toString();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+	
+	private String runClass() {
+		buffer = new StringBuffer();
+		
+		buffer.append("cmd.exe ");
+		buffer.append("/c ");
+		buffer.append("cppTest.exe");
+		
+		return buffer.toString();
+	}
+	
+	
+	
+	
+	public String execSave(String cmd) {
+		try {
+			process = Runtime.getRuntime().exec(cmd);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+}
+
+
+
+
+
+
+
+
+
+//public class RunTimeTest {
+//	public static void main(String[] args) {
+//		Cmd cmd = new Cmd();
+//		
+//		StringBuffer buffer = new StringBuffer();
+//		buffer.append("public class Test");
+//		buffer.append("{ public static void main(String[] args)");
+//		buffer.append("{ System.out.println(\"hi\");} }");
+//		
+//		String command = cmd.inputSource(buffer.toString());
+//		String result = cmd.execCommand(command);
+//		
+//		System.out.println(result);
+//	}
+//}
---
+++ compiletest/src/main/java/com/compile/compiletest/controller/CompileControllerCpp.java
@@ -0,0 +1,49 @@
+package com.compile.compiletest.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.compile.compiletest.Run.RunCpp;
+import com.compile.compiletest.dto.JsonResult;
+
+@Controller
+@RequestMapping("/compile")
+public class CompileControllerCpp {
+	@ResponseBody
+	@PostMapping("/cpp")
+	public JsonResult compileC(@RequestParam String code) {
+		RunCpp rtt = new RunCpp();
+
+		String result = rtt.execCommand();
+		
+		String errorResult = rtt.errorResult();
+		
+		
+		String[] res = new String[2];
+		res[0] = result;
+		res[1] = errorResult;
+		System.out.println("errorResult = " + errorResult);
+		
+		return JsonResult.success(res);
+	}
+	
+	@ResponseBody
+	@PostMapping("/cpp/save")
+	public JsonResult compileSaveC(@RequestParam String code) {
+		RunCpp rtt = new RunCpp();
+		
+		StringBuffer buffer = new StringBuffer();
+		String[] token = code.split("\n");
+		
+		for(int i = 0; i < token.length; i++) {
+			buffer.append(token[i] + "\n");
+		}
+		String command = rtt.inputSource(buffer.toString());
+		String result = rtt.execSave(command);
+		
+		return JsonResult.success(result);
+	}
+}
