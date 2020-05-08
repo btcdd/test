501f4a9ea3682db24e9233535cd2618f6829b182
박관우 <pgw9470@naver.com>
2020-05-08 11:47:02 +0900

    ㅁㄴㅇㄹ

--- compiletest/src/main/java/com/compile/compiletest/controller/CompileControllerCs.java
+++ compiletest/src/main/java/com/compile/compiletest/controller/CompileControllerCs.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.compile.compiletest.Run.RunCpp;
 import com.compile.compiletest.Run.RunCs;
 import com.compile.compiletest.dto.JsonResult;
 
