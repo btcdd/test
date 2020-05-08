6cd5b80ce485fa0d4c208212f3b0a9e92f332b12
jskim <jskim@naver.com>
2020-05-08 17:36:49 +0900

    join

--- compiletest/src/main/java/com/compile/compiletest/service/UserService.java
+++ compiletest/src/main/java/com/compile/compiletest/service/UserService.java
@@ -1,6 +1,12 @@
 package com.compile.compiletest.service;
 
+import java.util.Random;
+
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 import com.compile.compiletest.repository.UserRepository;
@@ -8,12 +14,46 @@ import com.compile.compiletest.vo.UserVo;
 
 @Service
 public class UserService {
+	
+	@Autowired
+	private JavaMailSender mailSender;
 
+	
 	@Autowired
 	private UserRepository userRepository;
 	
 	public boolean join(UserVo vo) {
 		return userRepository.insert(vo) == 1;
+	}
+
+	public String sendMail(String email,int tempKey) {
+
+		
+		try {
+			
+			MimeMessage message = mailSender.createMimeMessage();
+			MimeMessageHelper messageHelper = new MimeMessageHelper(message, true, "UTF-8");
+			messageHelper.setTo(email);
+			messageHelper.setText("인증번호 : "+tempKey);
+			messageHelper.setFrom("wltnfdlek@gmail.com");
+			messageHelper.setSubject("[회원가입 인증번호]");
+			
+			mailSender.send(message);
+
+		}catch(Exception e) {
+			System.out.println(e);
+		}
+		
+		return "success";
+
+
+
+	}
+
+	public int getTempKey() {
+		int random = new Random().nextInt(900000)+100000;
+		return random;
+		
 	}
 	
 }
