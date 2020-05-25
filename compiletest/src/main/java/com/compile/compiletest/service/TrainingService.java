e8ce3fa253edfd1313f5d9b6faf981aa8ac22c43
박관우 <pgw9470@naver.com>
2020-05-25 17:26:32 +0900

    트레이닝 페이징

--- compiletest/src/main/java/com/compile/compiletest/service/TrainingService.java
+++ compiletest/src/main/java/com/compile/compiletest/service/TrainingService.java
@@ -16,7 +16,7 @@ import com.compile.compiletest.vo.SubProblemVo;
 public class TrainingService {
 
 	private static final int postNum = 15; //한 페이지에 출력할 게시물 갯수
-	private static final int pageNum_cnt = 10; 		//한번에 표시할 페이징 번호의 갯수
+	private static final int pageNum_cnt = 5; 		//한번에 표시할 페이징 번호의 갯수
 	
 	@Autowired
 	private TrainingRepository trainingRepository;
@@ -77,14 +77,14 @@ public class TrainingService {
 		int displayPost = (currentPage -1) * postNum;
 		//표시되는 페이지 번호 중 마지막 번호
 		int endPageNum = (int)(Math.ceil((double)currentPage / (double)pageNum_cnt) * pageNum_cnt);
+		System.out.println(endPageNum);
 		//표시되는 페이지 번호 중 첫번째 번호
 		int startPageNum = endPageNum - (pageNum_cnt - 1);
 		//마지막번호 재계산
-		int endPageNum_tmp = (int)(Math.ceil((double)count / (double)pageNum_cnt));
+		int endPageNum_tmp = (int)(Math.ceil((double)count / (double)postNum));
 		if(endPageNum > endPageNum_tmp) {
 			endPageNum = endPageNum_tmp;
 		}
-		boolean prev = startPageNum == 1 ? false : true;//시작 페이지 번호가 1일 때를 제외하곤 무조건 출력
 		boolean next = endPageNum * pageNum_cnt >= count ? false : true;//마지막 페이지 번호가 총 게시물 갯수보다 작다면, 다음 구간이 있다는 의미이므로 출력
 		
 		List<ProblemVo> list;
@@ -107,8 +107,7 @@ public class TrainingService {
 		map.put("pageNum",pageNum);
 		map.put("select",currentPage);
 		map.put("startPageNum",startPageNum);
-		map.put("endPageNum",endPageNum + 1);		
-		map.put("prev",prev);
+		map.put("endPageNum",endPageNum + 1);
 		map.put("next",next);
 		map.put("keyword",keyword);
 		map.put("count", count);
