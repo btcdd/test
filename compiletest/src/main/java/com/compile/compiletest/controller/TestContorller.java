3d5ad6e12d33b7817e6273c8eaf33ca37a51c770
taesung <ktsung94@naver.com>
2020-05-29 09:28:59 +0900

    Coding Test 페이지 추가

--- compiletest/src/main/java/com/compile/compiletest/controller/TestContorller.java
+++ compiletest/src/main/java/com/compile/compiletest/controller/TestContorller.java
@@ -1,5 +1,7 @@
 package com.compile.compiletest.controller;
 
+import java.util.Calendar;
+import java.util.HashMap;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,13 +20,39 @@ public class TestContorller {
 
 	@Autowired
 	private TestService testService;
-	
+
 	@Auth
 	@RequestMapping(value="", method=RequestMethod.GET)
 	public String training(Model model) {
 		List<ProblemVo> list = testService.selectTestList();
 		model.addAttribute("list", list);
+
+		HashMap<Long, Long> map = new HashMap<Long, Long>();
+
+		Calendar today = Calendar.getInstance();
+		Calendar d = Calendar.getInstance();
+		today.set(Calendar.MONTH, today.get(Calendar.MONTH)+1);
+
+		String[] fake_token = null;
+		String[] token = null;
+
+		long l_today = today.getTimeInMillis() / (24*60*60*1000);
+		long l_dday = 0;
+		long result = 0;
 		
+		for(ProblemVo vo : list) {
+			if(vo.getPriority() == 2) {
+				fake_token = (vo.getStartTime()+"").split(" ");
+				token = fake_token[0].split("-");
+				d.set(Integer.parseInt(token[0]), Integer.parseInt(token[1]), Integer.parseInt(token[2]));
+				l_dday = d.getTimeInMillis() / (24*60*60*1000);
+				result = l_today - l_dday - 1;
+				map.put(vo.getNo(), result);
+			}
+		}
+		
+		model.addAttribute("dday", map);
+
 		return "test/list";
 	}
 }
