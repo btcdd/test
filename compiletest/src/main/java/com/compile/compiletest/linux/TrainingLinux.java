ebb827c1a58b780fc08b55232da08bbcd7766eab
박관우 <pgw9470@naver.com>
2020-06-03 09:12:52 +0900

    asdf

--- compiletest/src/main/java/com/compile/compiletest/linux/TrainingLinux.java
+++ compiletest/src/main/java/com/compile/compiletest/linux/TrainingLinux.java
@@ -6,7 +6,7 @@ public class TrainingLinux {
 	
 	public void saveProblemAndSubProblem(Long authUserNo, Long problemNo, Long[] subProblemArray) {
 		try {
-			process = Runtime.getRuntime().exec("mkdir userDirectory/user" + authUserNo + "/prob" + problemNo + "/\n");
+			process = Runtime.getRuntime().exec("mkdir userDirectory/user" + authUserNo + "/prob" + problemNo + "\n");
 			for(int i = 0; i < subProblemArray.length; i++) {
 				process = Runtime.getRuntime().exec("mkdir userDirectory/user" + authUserNo + "/prob" + problemNo + "/subProb" + subProblemArray[i] + "\n");
 			}
