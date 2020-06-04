34dc382bf84e3aa773d26462c00e93b47a55a702
jskim <jskim@naver.com>
2020-06-04 10:03:12 +0900

    ddd

--- compiletest/src/main/java/com/compile/compiletest/controller/api/CodeTreeController.java
+++ compiletest/src/main/java/com/compile/compiletest/controller/api/CodeTreeController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.compile.compiletest.dto.JsonResult;
@@ -42,20 +41,16 @@ public class CodeTreeController {
 		boolean exist = trainingService.existUser(userEmail); //유저가 있는지 체크
 		if(!exist || userEmail=="undifiend") {
 			System.out.println("유저가 존재하지 않는다!!!");
-			
 			map.put("result", "emptyUser");
-			return JsonResult.success(map); //redirect 홈으로(리액트에서)
+			return JsonResult.success(map);
 		}
 		System.out.println("_authUser>>>>"+_authUser);
 		if(_authUser == null) {
 			System.out.println("잘못된 접근 경로 !!!");
 			map.put("result", "wrongAccess");
-			return JsonResult.success(map); //redirect 홈으로(리액트에서)
+			return JsonResult.success(map);
 		}
 		
-		//1.만약에 userEmail에 회원가입이 되어있지 않는 사용자일 경우 처리
-		//2.userEmail이 즐겨찾기 한 문제 리스트들이 쭉 뜰수 있는 정보가 필요
-		//3.문제 리스트 중 하나를 클릭하면 해당하는 패키지 상세가 뜰 수 있도록 하는 세부 문제 정보가 필요
 		
 		
 		System.out.println("!!!!");
