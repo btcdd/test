e84bc8bb2d36fa52cbee483a50b1a90662b94913
박관우 <pgw9470@naver.com>
2020-06-08 19:30:07 +0900

    dd

--- compiletest/src/main/java/com/compile/compiletest/runlanguage/RunJavaCodeTree.java
+++ compiletest/src/main/java/com/compile/compiletest/runlanguage/RunJavaCodeTree.java
@@ -42,7 +42,7 @@ public class RunJavaCodeTree {
 	public void createFileAsSource(String source, String fileName) {
 		try {
 			file = new File("/userDirectory/user" + authUserNo + "/prob" + problemNo + "/subProb" + subProblemNo + "/java/" + fileName);
-			bufferWriter = new BufferedWriter(new FileWriter(file, false));
+			bufferWriter = new BufferedWriter(new FileWriter(file));
 			
 			bufferWriter.write(source);
 			bufferWriter.flush();
