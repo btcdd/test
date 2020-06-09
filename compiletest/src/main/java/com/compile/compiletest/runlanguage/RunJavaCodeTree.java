d88307c518920ebce8a9349ab977897c7edb3b33
y00jin <59616862+y00jin@users.noreply.github.com>
2020-06-09 09:30:15 +0900

    냐냐

--- compiletest/src/main/java/com/compile/compiletest/runlanguage/RunJavaCodeTree.java
+++ compiletest/src/main/java/com/compile/compiletest/runlanguage/RunJavaCodeTree.java
@@ -34,7 +34,7 @@ public class RunJavaCodeTree {
 		
 		buffer = new StringBuffer();
 		
-		buffer.append("javac -d /userDirectory/user" + authUserNo + "/prob" + problemNo + "/subProb" + subProblemNo + "/java/ *.java");
+		buffer.append("javac -d /userDirectory/user" + authUserNo + "/prob" + problemNo + "/subProb" + subProblemNo + "/java/ /userDirectory/user" + authUserNo + "/prob" + problemNo + "/subProb" + subProblemNo + "/java/*.java");
 		
 		return buffer.toString();
 	}
@@ -44,7 +44,7 @@ public class RunJavaCodeTree {
 			file = new File(fileName);
 			bufferWriter = new BufferedWriter(new FileWriter(file, false));
 			
-			bufferWriter.write("package subProb" + subProblemNo + "\n\n" + source);
+			bufferWriter.write("package subProb" + subProblemNo + ";\n\n" + source);
 			bufferWriter.flush();
 		} catch(Exception e) {
 			e.printStackTrace();
